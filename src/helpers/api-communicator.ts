1ac7bb3c333bf0a5f14effc2df61d58d7b21fe32
SunkaraboinaPraveenKumar <praveenkumargitsunkaraboina@gmail.com>
2024-08-03 14:26:52 +0530

    Front End changes

--- src/helpers/api-communicator.ts
+++ src/helpers/api-communicator.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3000/api/v1";
+axios.defaults.baseURL = "https://backendaibotmern-5.onrender.com/api/v1";
 axios.defaults.withCredentials = true;
 
 export const loginUser = async (email: string, password: string) => {
